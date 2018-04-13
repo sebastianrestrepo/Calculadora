068cbe43469598bd4cb152ca5e14813392740fc9
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2018-04-12 23:47:01 -0500

    :octocat: working!

--- index.js
+++ index.js
@@ -5,12 +5,74 @@ app.get('/', (req, res) => res.send('CALCULADORA!'));
 
 app.listen(5050, () => console.log('Example app listening on port 5050!'));
 
+//sumar
 app.get('/suma', function (req, res) {
-    if(!req.query.a&!req.query.b){
-        res.send('Faltan parametros');
-    } else{
-        res.send('El resultado de la suma: ' + a + b);
-        var {a, b} = req.query;
+
+    if (Object.keys(req.query).length === 0) {
+        console.log("no hay nada");
+        res.send('No hay nada');
+    } else if (Object.keys(req.query).length === 2) {
+        var {
+            a,
+            b
+        } = req.query;
+        console.log(a + b);
+        console.log(req.query);
+        res.send('El resultado de la suma es: ' + parseInt(a) + parseInt(b));
     }
-    res.send('SUMA');
-  });
+
+});
+
+//resta
+app.get('/resta', function (req, res) {
+
+    if (Object.keys(req.query).length === 0) {
+        console.log("no hay nada");
+        res.send('No hay nada');
+    } else if (Object.keys(req.query).length === 2) {
+        var {
+            a,
+            b
+        } = req.query;
+        console.log(a - b);
+        console.log(req.query);
+        res.send('El resultado de la resta es: ' + (parseInt(a) - parseInt(b)));
+    }
+
+});
+
+//multiplicación
+app.get('/multiplicacion', function (req, res) {
+
+    if (Object.keys(req.query).length === 0) {
+        console.log("no hay nada");
+        res.send('No hay nada');
+    } else if (Object.keys(req.query).length === 2) {
+        var {
+            a,
+            b
+        } = req.query;
+        console.log(a * b);
+        console.log(req.query);
+        res.send('El resultado de la multiplicación es: ' + (parseInt(a) * parseInt(b)));
+    }
+
+});
+
+//division
+app.get('/division', function (req, res) {
+
+    if (Object.keys(req.query).length === 0) {
+        console.log("no hay nada");
+        res.send('No hay nada');
+    } else if (Object.keys(req.query).length === 2) {
+        var {
+            a,
+            b
+        } = req.query;
+        console.log(a / b);
+        console.log(req.query);
+        res.send('El resultado de la división es: ' + (parseInt(a) / parseInt(b)));
+    }
+
+});
